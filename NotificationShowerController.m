cc068fa6986dee8992a3164f0a4b892150e24468
Daniel Ferreira (theiostream) <daniel.calibeta@gmail.com>
2013-02-07 15:50:59 -0200

    Fix iPhone framing/file

--- NotificationShowerController.m
+++ NotificationShowerController.m
@@ -2,7 +2,7 @@
 #import <UIKit/UIKit.h>
 #import "BBWeeAppController-Protocol.h"
 
-#define VIEW_HEIGHT 24.0f
+#define VIEW_HEIGHT 28.0f
 
 @interface UIDevice (theiostream)
 - (BOOL)isWildcat;
@@ -12,7 +12,6 @@
 {
 	UIView *_view;
 	UITextField *textField;
-	NSMutableDictionary *dict;
 }
 
 @end
@@ -25,33 +24,28 @@
 	[super dealloc];
 }
 
-- (id)init
-{
-	if ((self = [super init])) {}
-	return self;
-}
-
 - (UIView *)view
 {
 	if (!_view)
 	{
 		_view = [[UIView alloc] initWithFrame:CGRectMake(2.0f, 0.0f, 316.0f, VIEW_HEIGHT)];
 		
-		dict = [[NSMutableDictionary alloc] initWithContentsOfFile:@"/var/mobile/Library/Preferences/am.theiostre.notificationshower.plist"];
+		NSString *string = [NSString stringWithContentsOfFile:@"/var/mobile/Library/notificationshower_i_am_sorry.txt" encoding:NSUTF8StringEncoding error:NULL];
+		
 		UIImage *bgImg = [[UIImage imageWithContentsOfFile:@"/System/Library/WeeAppPlugins/NotificationShower.bundle/WeeAppBackground.png"] stretchableImageWithLeftCapWidth:5 topCapHeight:71];
         UIImageView *bg = [[UIImageView alloc] initWithImage:bgImg];
-        bg.frame = [[UIDevice currentDevice] isWildcat] ? CGRectMake(15.0f, 0.0f, 440.0f, VIEW_HEIGHT) : CGRectMake(4.0f, 0.0f, 316.0f, VIEW_HEIGHT);
+        bg.frame = [[UIDevice currentDevice] isWildcat] ? CGRectMake(4.0f, 0.0f, 470.0f, VIEW_HEIGHT) : CGRectMake(4.0f, 0.0f, 312.0f, VIEW_HEIGHT);
         [_view addSubview:bg];
         [bg release];
 		
-		CGRect frame = [[UIDevice currentDevice] isWildcat] ? CGRectMake(20.0f, 0.0f, 430.0f, VIEW_HEIGHT) : CGRectMake(4.0f, 0.0f, 316.0f, VIEW_HEIGHT);
+		CGRect frame = [[UIDevice currentDevice] isWildcat] ? CGRectMake(4.0f, 2.0f, 470.0f, VIEW_HEIGHT-4.f) : CGRectMake(4.0f, 2.0f, 312.0f, VIEW_HEIGHT-4.f);
 		textField = [[UITextField alloc] initWithFrame:frame];
 		textField.backgroundColor = [UIColor clearColor];
 		textField.textColor = [UIColor whiteColor];
-		if (![dict objectForKey:@"Text"])
+		if (string == nil)
 			textField.text = @"Type something.";
 		else
-			textField.text = [dict objectForKey:@"Text"];
+			textField.text = string;
 		textField.font = [UIFont fontWithName:@"Verdana" size:18.0];
 		textField.textAlignment = UITextAlignmentCenter;
 		textField.delegate = self;
@@ -62,11 +56,11 @@
 }
 
 - (void)textFieldDidEndEditing:(UITextField *)anotherTextField {
-	[dict setObject:[anotherTextField text] forKey:@"Text"];
-	[dict writeToFile:@"/var/mobile/Library/Preferences/am.theiostre.notificationshower.plist" atomically:YES];
+	[[anotherTextField text] writeToFile:@"/var/mobile/Library/notificationshower_i_am_sorry.txt" atomically:YES encoding:NSUTF8StringEncoding error:NULL];
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)aTextField {
+	NSLog(@"yay2");
 	[aTextField resignFirstResponder];
 	return YES;
 }
